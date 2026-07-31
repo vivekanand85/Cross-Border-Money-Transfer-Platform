6dc193f8d3f4e971db6f17d2ea6d53b571119f14
vivekanand85 <vivekanandkadole18@gmail.com>
2026-07-31 10:20:56 +0530

    Fix: wire Testcontainers Postgres into both ApplicationTests classes for CI

--- ledger-service/src/test/java/com/moneytransfer/ledger_service/LedgerServiceApplicationTests.java
+++ ledger-service/src/test/java/com/moneytransfer/ledger_service/LedgerServiceApplicationTests.java
@@ -2,12 +2,33 @@ package com.moneytransfer.ledger_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class LedgerServiceApplicationTests {
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.3"))
+			.withDatabaseName("ledger_db")
+			.withUsername("ledger_user")
+			.withPassword("ledger_pass");
+
+	static {
+		postgres.start();
+	}
+
+	@DynamicPropertySource
+	static void overrideDatasourceProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
